99372f3bccd6a81228179d2c25f36ea0dc24e400
srinivasgurrala <srinivasgurrala@gmail.com>
2021-03-14 14:25:39 +0530

    Add files via upload

---
+++ RPractice.R
@@ -0,0 +1,41 @@
+
+1+1
+
+2+3*4
+
+3^2
+
+
+sqrt(10)
+
+###############################
+x <- 1
+y <-4
+z<- 5
+
+x*y*z
+#Creation of vectors
+Age <- c(22,21,24,26)
+
+x<-c(2,0,0,4)
+y<-c(1,9,9,9)
+
+#Addition of vectors
+x+y
+
+#Scalr mult of vectors
+x*4
+
+#Applying sqrt function on vector
+sqrt(x)
+
+#Accessing the vector elements
+
+##Data Frame
+
+df<- data.frame(x=1:3,y=c("a","b","c"))
+
+
+
+
+
